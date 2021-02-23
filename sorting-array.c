2b86b7ddc63e9c6a3bb57dda16fe9c357234b7e8
Alexvc23 <alexandervalencia1994@gmail.com>
2021-02-23 15:02:47 +0100

    all function completed
    program already tested and updated to HackerRank

--- sorting-array.c
+++ sorting-array.c
@@ -2,58 +2,78 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+int checkInput(char ** a, int n)
+{
+	for(int i = 0; i < n; i++)
+	{
+		for(int j = 0; j < strlen(a[i]); j++)
+		{
+			if((a[i][j] < 'a' ) || (a[i][j] > 'z' && a[i][j] < 'a') || (a[i][j] > 'z'))
+			{
+				return 1;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+	}
+}
+
+
 void swap(char ** a, char ** b)
 {
-	char *temp = *a;
-	*a = *b;
-	*b = temp;
+    char *temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
 // function to pass values by reference, comparing both strings and returning the difference when the first unmatch character appears
 int lexicographic_sort(const char* a, const  char* b) 
 {
-	return strcmp(a, b);
+    return strcmp(a, b);
 }
 
 /* the same as lexicographic_sort function, with the difference of comparing backwards "b against a".*/
 int lexicographic_sort_reverse(const char* a, const char* b) 
 {
-	return strcmp(b, a);
+    return strcmp(b, a);
 }
 
 int distinct_characters(const char* a)
 {
-	// Store number of characters alphabetically
-	int alphabet[25];
+    // store number of characters alphabetically
+    int alphabet[25];
 
-	// Initicalize alphabet indexes to 0
-	for(int i = 0; i < sizeof(alphabet)/sizeof(int); i++)
-	{
-		alphabet[i] = 0;
-	}
+    // initicalize alphabet indexes to 0
+    for(int i = 0; i <= sizeof(alphabet)/sizeof(int); i++)
+    {
+        alphabet[i] = 0;
+    }
 
-	// Loop to add up number of characters alphabetically
-	while(*a != '\0')
-	{
-		if((*a >= 'a') || (*a <= 'z') || (*a >= 'A') || (*a <= 'Z'))
-		{
-			alphabet[*a - 'a'] += 1;
-		}
+    // loop to add up number of characters alphabetically
+    while(*a != '\0')
+    {
+        if((*a >= 'a') || (*a <= 'z') || (*a >= 'a') || (*a <= 'z'))
+        {
+            alphabet[*a - 'a'] += 1;
+        }
 
-		*a++;
-	}	
+        *(a++);
+    }    
 
-	int count = 0;
-	// Loop to count number of distinct characters in alphabet array
-	for(int i = 0; i < sizeof(alphabet)/sizeof(int); i++)
-	{
-		if(alphabet[i] > 0)
-		{
-			count++;
-		}
-	}
+    int count = 0;
+    // loop to count number of distinct characters in alphabet array
+    for(int i = 0; i <= sizeof(alphabet)/sizeof(int); i++)
+    {
+        if(alphabet[i] > 0)
+        {
+            count++;
+        }
+    }
 
-	return count;
+    return count;
 }
 
 
@@ -61,38 +81,56 @@ int distinct_characters(const char* a)
 int sort_by_number_of_distinct_characters(const char* a, const char* b) 
 {
 
-	int distA = distinct_characters(a); 		
-	int distB = distinct_characters(b);
-	
-	if((distA - distB) != 0)
-	{
-		return (distA - distB);
-	}
-	else
-	{
-		return strcmp(a, b);
-	}
-	    
+    int dista = distinct_characters(a);         
+    int distb = distinct_characters(b);
+	int result = (distinct_characters(a) - distinct_characters(b));
+    
+    // detemine sorting by distinct characters or lexicographiclly     
+    if(result != 0)
+    {
+        return result;
+    }
+    else
+    {
+        return strcmp(a, b);
+    }
+        
 }
 
 int sort_by_length(const char* a, const char* b) 
 {
+    int lenghta = strlen(a);
+    int lenghtb = strlen(b);
+    int result = (lenghta - lenghtb);
+    
+    if(result != 0)
+    {
+        return result;
+    }
+    else
+    {
+        return lexicographic_sort(a, b); 
+ 	}        
+
 
 }
 
-// function to sort all sort function using swap function and passing **arr as reference
 void string_sort(char** arr,const int len,int (*cmp_func)(const char* a, const char* b))
 {
-	for(int i = 0; i < len - 1; i++)
+	if(checkInput(arr, len))
 	{
-		for(int j = 0;j < len - i - 1; j++)
-		{
-			if(cmp_func(arr[j], arr[j+1]) > 0)
-			{
-				swap(&arr[j], &arr[j+1]);
-			}
-		}
+		exit(0);
 	}
+    for(int i = 0; i < len - 1; i++)
+    {
+        for(int j = 0;j < len - i - 1; j++)
+        {
+            if(cmp_func(arr[j], arr[j+1]) > 0)
+            {
+                swap(&arr[j], &arr[j+1]);
+            }
+        }
+    }
 
 }
 
@@ -110,7 +148,8 @@ int main()
         scanf("%s", *(arr + i));
         *(arr + i) = realloc(*(arr + i), strlen(*(arr + i)) + 1);
     }
-
+	
+	printf("\n\n");
 
     string_sort(arr, n, lexicographic_sort);
     for(int i = 0; i < n; i++)
@@ -122,12 +161,10 @@ int main()
         printf("%s\n", arr[i]); 
     printf("\n");
 
-	/*
     string_sort(arr, n, sort_by_length);
     for(int i = 0; i < n; i++)
         printf("%s\n", arr[i]);    
     printf("\n");
-	*/
 
     string_sort(arr, n, sort_by_number_of_distinct_characters);
     for(int i = 0; i < n; i++)
